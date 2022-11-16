9a8b3e45f799ea5bf14c98cffcb6034dc918edd6
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2022-11-16 20:05:00 -0300

    roy

---
+++ Aula16.11/geti.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>aaaaaaaaa</title>
+</head>
+<body>
+	<h2>Formulário - Recuperando dados - GET - URL</h2>
+	<a href="passando_dadu.php?id=4&user=jhin">passa</a>
+
+</body>
+</html>
---
+++ Aula16.11/passando_dadu.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo "<h3>Recuperando os dados passados via GET por URL<h3/>";
+
+echo "ID: ".$_REQUEST['id'];
+echo "<br>";
+echo "User: ".$_REQUEST['user'];
+
+ ?>
