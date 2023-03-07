e430b1b897e3a6cfe3712a3de0a5a3e4ed206591
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2023-03-06 22:04:45 -0300

    fome

--- 3ºModulo/Aula02.27/cria_algo_ai.php
+++ 3ºModulo/Aula02.27/cria_algo_ai.php
@@ -3,10 +3,22 @@
 	include_once 'ex1.php';
 	
 	$valor = new Produto();
+	$valor1 = new Produto();
 
 	$valor->Codigo=911;
+	$valor1->Codigo=191;
+	$valor->Preco=10;
+	$valor->Quantidade=7;
+	$valor->Porcentagem=50;
+	$valor1->Preco=20;
+	$valor1->Quantidade=6;
+	$valor1->Porcentagem=50;
 
-	$valor->Descricao="policia aqui mané";
+	$valor->Descricao="policia americana aqui mané";
+	$valor1->Descricao="policia Br aqui mané";
 
-	echo $valor->Codigo . " - " . $valor->Descricao;
+	$valor->Desconto();
+	$valor1->Desconto();
+	$valor->ImprimeEtiqueta();
+	$valor1->ImprimeEtiqueta(); 
  ?>
