e9370af9e67d2a06ec5bc5edb71eb36446aa246c
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2023-03-22 21:12:48 -0300

    a

--- 3ºModulo/Aula03.01/dor.php
+++ 3ºModulo/Aula03.01/dor.php
@@ -27,8 +27,22 @@
 			if ($anos > 0) {
 				$this->idade += $anos;
 			}
+	
 		}
+		function __construct($codigo, $nome, $altura, $idade, $nascimento, $escolaridade, $salario)
+	{
+		$this->codigo = $codigo;
+		$this->nome = $nome;
+		$this->altura = $altura;
+		$this->idade = $idade;
+		$this->nascimento = $nascimento;
+		$this->escolaridade = $escolaridade;
+		$this->salario = $salario;
 	}
+
+
+	}
+
 
 
 
