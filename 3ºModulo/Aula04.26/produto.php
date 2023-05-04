a5b13062140e7d990d7e82e14dc5b410cd7bf099
AndreNeves30 <101944380+AndreNeves30@users.noreply.github.com>
2023-05-03 21:54:19 -0300

    vetor

---
+++ 3ºModulo/Aula04.26/produto.php
@@ -0,0 +1,15 @@
+<?php
+
+class Produto {
+    var $Codigo;
+    var $Descricao;
+    var $Preco;
+    var $Quantidade;
+
+    function ImprimirEtiqueta(){
+        print 'Codigo: ' . $this-> Codigo. "<br>";
+        print 'Descrição: ' . $this-> Descricao. "<br><br>";
+    }
+}
+
+?>
